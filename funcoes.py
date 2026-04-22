07163346c9912712aefe5ba119d163af08856804
Melissa <melissask1@al.insper.edu.br>
2026-04-22 17:59:28 -0300

    Questao 1

---
+++ funcoes.py
@@ -0,0 +1,11 @@
+import random
+
+def rolar_dados(quantidade):
+    resultados = []
+
+    for _ in range(quantidade):
+        
+        dado = random.randint(1, 6)
+        resultados.append(dado)
+
+    return resultados
