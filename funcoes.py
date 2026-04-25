c6636fb2d611bd952a984ec6ea272ec73dd25f35
Melissa <melissask1@al.insper.edu.br>
2026-04-25 18:33:49 -0300

    Ex 2

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,13 @@ def rolar_dados(quantidade):
         resultados.append(dado)
 
     return resultados
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+ 
+    dado = dados_rolados.pop(dado_para_guardar)
+    
+
+    dados_no_estoque.append(dado)
+    
+    
+    return [dados_rolados, dados_no_estoque]
