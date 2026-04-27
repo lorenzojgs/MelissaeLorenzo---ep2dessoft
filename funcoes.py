33b838861c3405613b9ab8ebd5d7f0d174fa92f3
Melissa <melissask1@al.insper.edu.br>
2026-04-27 10:22:54 -0300

    ex 6

--- funcoes.py
+++ funcoes.py
@@ -51,3 +51,37 @@ def calcula_pontos_sequencia_baixa(dados):
     if {1, 2, 3, 4}.issubset(valores) or {2, 3, 4, 5}.issubset(valores) or {3, 4, 5, 6}.issubset(valores):
         return 15
     return 0
+
+def calcula_pontos_regra_simples(dados):
+    pontos = {}
+    
+    for i in range(1, 7):
+        pontos[i] = dados.count(i) * i
+    
+    return pontos
+
+def calcula_pontos_soma(dados):
+    soma = 0
+    for valor in dados:
+        soma += valor
+    return soma
+
+def calcula_pontos_sequencia_baixa(dados):
+    sequencias = [
+        [1, 2, 3, 4],
+        [2, 3, 4, 5],
+        [3, 4, 5, 6]
+    ]
+    
+    for seq in sequencias:
+        encontrou = True
+        for numero in seq:
+            if numero not in dados:
+                encontrou = False
+                break
+        if encontrou:
+            return 15
+    
+    return 0
+
+
